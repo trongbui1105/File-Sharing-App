bbce6e1ce2b9f24344d3bd1740814d932efb4489
trongbui1105 <buiquoctrong110500@gmail.com>
2021-12-29 11:24:48 +0700

    Add Cors

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(express.json());
 
 const connectDB = require('./config/db');
 connectDB();
 
-app.use(express.static('public'));
-app.use(express.json());
+// Cors
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(','),
+}
+app.use(cors(corsOptions));
+
 // Template engine
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
